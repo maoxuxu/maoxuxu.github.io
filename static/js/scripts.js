fff7c73a261158237e929ca015b365db1c1d5cf7
maoxuxu <152969779+maoxuxu@users.noreply.github.com>
2026-09-24 14:20:02 +0800

    Add files via upload

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards', 'academicservice']
+const section_names = ['home', 'publications', 'awards', 'academicservice', 'news']
 
 
 window.addEventListener('DOMContentLoaded', event => {
